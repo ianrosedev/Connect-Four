0e5654c2eea068d811ecd80120e2203d82d9a302
Ian Rose <ianrosedev@gmail.com>
2016-12-09 19:41:57 -0500

    wip - making tokens fall

--- scripts/main.js
+++ scripts/main.js
@@ -3,19 +3,29 @@ let MasterArray = [],
     Player2 = [];
     color = 'red';
 
+
+// Fills in  MasterArray
 (function populateMasterArray() {
   // Row
   for (let i = 0; i < 7; i ++) {
     MasterArray.push([]);
     // Column
     for (let j = 0; j < 6; j++) {
-      MasterArray[i].push(j);
+      MasterArray[i].push(null);
     }
   }
 })();
 
-$('#user-submit').on('click', function() {
-  $('#foo').addClass('red');
-  $('#cpu-color').text(color);
-  console.log(color);
-});
+$('#cpu-color').text(`Choose a spot, ${color}`);
+
+$('#row1').on('click', row1);
+
+function row1() {
+  for (let i = MasterArray[0].length - 1; i >= 0; i--) {
+    if (MasterArray[0][i] === null) {
+      MasterArray[0][i] = color;
+      $('#0' + i).addClass(color);
+      return;
+    }
+  }
+}
