74074aeb8901eb3a16c4dbb8178ccf65119c57ea
Ian Rose <ianrosedev@gmail.com>
2016-12-15 23:56:37 -0500

    test over

--- index.js
+++
@@ -1,8 +0,0 @@
-const static = require('node-static');
-const file = new static.Server();
-
-require('http').createServer(function(req, res) {
-  req.addListener('end', function() {
-    file.serve(req, res);
-  }).resume();
-}).listen(process.env.PORT || 3000);
